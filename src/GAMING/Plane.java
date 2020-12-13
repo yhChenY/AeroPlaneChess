8b660c39e91f6ac63637b3e83b2e71b868699da4
yhChenY <1281807522@qq.com>
2020-12-13 22:27:39 +0800

    bala

--- src/GAMING/Plane.java
+++ src/GAMING/Plane.java
@@ -9,73 +9,75 @@ public class Plane {
   private State state;
   private final Color color;
   private Block position;
-  private boolean hasSetOff=false;
+  private boolean hasSetOff = false;
   private JButton button;
   
   public Plane(Color color) {
+    button = new JButton();
     this.color = color;
-    state= State.FORWARD;
+    state = State.FORWARD;
     button.setIcon(new ImageIcon("resources/" + color.getColor() + "Airplane.png"));
     button.setFocusPainted(false);
     button.addMouseListener(new MouseAdapter() {
       @Override
       public void mouseClicked(MouseEvent e) {
         super.mouseClicked(e);
-        TODO;
+        run(Main.getChosenStep());
       }
     });
   }
   
-  public void setOff(){
-    hasSetOff=true;
+  public void setOff() {
+    hasSetOff = true;
+    
   }
   
   public boolean isHasSetOff() {
     return hasSetOff;
   }
   
-  public void setPosition(Block block){
-    this.position=block;
+  public void setPosition(Block block) {
+    this.position = block;
   }
   
   public Block getPosition() {
     return position;
   }
   
-  public boolean isForward(){
-    return state== State.FORWARD;
+  public boolean isForward() {
+    return state == State.FORWARD;
   }
   
-  public boolean isBackward(){
+  public boolean isBackward() {
     return !isForward();
   }
   
-  public void setForward(){
-    this.state= State.FORWARD;
+  public void setForward() {
+    this.state = State.FORWARD;
   }
   
-  public void setBackward(){
-    this.state= State.BACKWARD;
+  public void setBackward() {
+    this.state = State.BACKWARD;
   }
   
   public Color getColor() {
     return color;
   }
-
+  
   public JButton getButton() {
     return button;
   }
-
-  public void run(int n){
-    Block dest=position.getNextNBlock(n,this);
+  
+  public void run(int n) {
+    Block dest = position.getNextNBlock(n, this);
     setPosition(dest);
-    if(dest.getColor()==color&&dest.getType()== Block.Type.COMMON){
-      dest=dest.getNextNBlock(4,this);
+    if (dest.getColor() == color && dest.getType() == Block.Type.COMMON) {
+      dest = dest.getNextNBlock(4, this);
       setPosition(dest);
       return;
     }
-    if(dest.getColor()==color&&dest.getType()== Block.Type.FLY){
-      dest=dest.getFlyBlock();
+    if (dest.getColor() == color && dest.getType() == Block.Type.FLY) {
+      dest = dest.getFlyBlock();
       setPosition(dest);
       return;
     }
@@ -85,5 +87,5 @@ public class Plane {
   enum State {
     FORWARD, BACKWARD
   }
-
+  
 }
