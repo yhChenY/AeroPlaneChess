e5c49d4b7894527fe291b46ec56e8f62d9d91fa2
dazhi0619 <da.zhi0619@gmail.com>
2020-12-20 16:29:48 +0800

    修复了投降后未重置飞机位置的bug

--- src/gui/Game.java
+++ src/gui/Game.java
@@ -127,6 +127,7 @@ public class Game extends JFrame {
         super.mouseClicked(e);
         bgm.stop();
         dispose();
+        Main.initializeData();
         //resetServer();
         new MainMenu();
       }
